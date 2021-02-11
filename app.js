86aca2e5d18baf4b2bbeeccc09a56c38874bcd6d
Jelson J <jelson20@outlook.com>
2021-02-11 14:56:25 +0000

    commi html update

---
+++ app.js
@@ -0,0 +1,10 @@
+var myName = 'sabado';
+console.log(myName);
+var isfalse = false;
+var age = 35;
+age = 23;
+console.log(age);
+var circ = function (dir) {
+    return dir * Math.PI;
+};
+console.log(circ('hello'));
---
+++ app.ts
@@ -0,0 +1,12 @@
+
+const myName = 'sabado'
+console.log(myName)
+let isfalse = false
+let age = 35
+age = 23
+console.log(age)
+
+const circ = (dir) => {
+  return dir * Math.PI
+}
+console.log(circ('hello'))
