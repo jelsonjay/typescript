a8ac44cec7b3260f050398ea8fe7b3019daa1324
Jelson J <jelson20@outlook.com>
2021-02-11 16:45:57 +0000

    commit update

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-var myName = 'sabado';
-console.log(myName);
-var isfalse = false;
-var age = 35;
-age = 23;
-console.log(age);
-var circ = function (dir) {
-    return dir * Math.PI;
-};
-console.log(circ('hello'));
+// varable
+var myName;
+var age;
+// array
+var isBoo;
+var contries = [];
+contries.push('Luanda');
+contries.push('London');
+contries.push('Leeds');
+console.log(contries);
+//union types
+var mix = [];
+mix.push(8);
+mix.push('england');
+var mynew;
+mynew = 8;
+console.log(mynew);
+console.log(mix);
+// objects
+var mixe;
+mixe = { name: 'jo', age: 85 };
+console.log(mixe);
+// any
+var ano = 12;
+ano = true;
+console.log(ano, '========Boolean========');
+ano = 'april';
+console.log(ano, '======String======');
+ano = ['banana', 'apple', 'orange'];
+console.log(ano, '====Array====');
+ano = { name: 'jemol', age: 9, job: 'student' };
+console.log(ano, '========Object=========');
--- app.ts
+++ app.ts
@@ -1,12 +1,36 @@
 
-const myName = 'sabado'
-console.log(myName)
-let isfalse = false
-let age = 35
-age = 23
-console.log(age)
+// varable
+let myName: string;
+let age: number
 
-const circ = (dir) => {
-  return dir * Math.PI
-}
-console.log(circ('hello'))
+// array
+let isBoo: true
+let contries:string[] = []
+contries.push('Luanda')
+contries.push('London')
+contries.push('Leeds')
+console.log(contries)
+
+//union types
+let mix:(string | number)[] = []
+mix.push(8)
+mix.push('england')
+let mynew: number|string
+mynew = 8
+console.log(mynew)
+console.log(mix)
+// objects
+let mixe: object
+mixe = {name: 'jo' , age: 85}
+console.log(mixe)
+
+// any
+let ano: any = 12
+ano = true
+console.log(ano, '========Boolean========')
+ano = 'april'
+console.log(ano, '======String======')
+ano = ['banana', 'apple', 'orange']
+console.log(ano, '====Array====')
+ano =  {name:'jemol', age:9, job: 'student'}
+console.log(ano, '========Object=========')
