533566c6e85373bbdfc37f753139b6bde0236307
Jelson J <jelson20@outlook.com>
2021-02-11 17:04:24 +0000

    commit update

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-// varable
+//=======varable======
 var myName;
 var age;
-// array
+//=======array========
 var isBoo;
 var contries = [];
 contries.push('Luanda');
 contries.push('London');
 contries.push('Leeds');
 console.log(contries);
-//union types
+//=====union types====
 var mix = [];
 mix.push(8);
 mix.push('england');
@@ -16,17 +16,17 @@ var mynew;
 mynew = 8;
 console.log(mynew);
 console.log(mix);
-// objects
+//======objects=====
 var mixe;
 mixe = { name: 'jo', age: 85 };
 console.log(mixe);
-// any
+// =====any=========
 var ano = 12;
 ano = true;
-console.log(ano, '========Boolean========');
+console.log(ano, '=========Boolean========');
 ano = 'april';
-console.log(ano, '======String======');
+console.log(ano, '========String======');
 ano = ['banana', 'apple', 'orange'];
-console.log(ano, '====Array====');
+console.log(ano, '========Array=======');
 ano = { name: 'jemol', age: 9, job: 'student' };
 console.log(ano, '========Object=========');
--- app.ts
+++ app.ts
@@ -1,9 +1,9 @@
 
-// varable
+//=======varable======
 let myName: string;
 let age: number
 
-// array
+//=======array========
 let isBoo: true
 let contries:string[] = []
 contries.push('Luanda')
@@ -11,7 +11,7 @@ contries.push('London')
 contries.push('Leeds')
 console.log(contries)
 
-//union types
+//=====union types====
 let mix:(string | number)[] = []
 mix.push(8)
 mix.push('england')
@@ -19,18 +19,18 @@ let mynew: number|string
 mynew = 8
 console.log(mynew)
 console.log(mix)
-// objects
+//======objects=====
 let mixe: object
 mixe = {name: 'jo' , age: 85}
 console.log(mixe)
 
-// any
+// =====any=========
 let ano: any = 12
 ano = true
-console.log(ano, '========Boolean========')
+console.log(ano, '=========Boolean========')
 ano = 'april'
-console.log(ano, '======String======')
+console.log(ano, '========String======')
 ano = ['banana', 'apple', 'orange']
-console.log(ano, '====Array====')
+console.log(ano, '========Array=======')
 ano =  {name:'jemol', age:9, job: 'student'}
 console.log(ano, '========Object=========')
